80b26dedafb00842a43bf5d15b60ebb12fe11186
Jelle Baele <jellebaele@hotmail.com>
2024-08-18 20:00:38 +0200

    Implement quicksort

---
+++ src/quick_sort/QuickSort.ts
@@ -0,0 +1,20 @@
+// O(n^2) worst case
+// O(n logn) average
+
+export default class QuickSort<T> {
+  public sort(arr: Array<T>): Array<T> {
+    if (arr.length < 2) return arr;
+
+    const pivot = arr[0];
+    const partitionLowerNumbers = [];
+    const partationHigherNumbers = [];
+
+    for (let i = 1; i < arr.length; i++) {
+      const element = arr[i];
+      if (element < pivot) partitionLowerNumbers.push(element);
+      else partationHigherNumbers.push(element);
+    }
+
+    return [...this.sort(partitionLowerNumbers), pivot, ...this.sort(partationHigherNumbers)];
+  }
+}
