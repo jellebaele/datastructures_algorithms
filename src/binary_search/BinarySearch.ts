43e14a041f385f2bc429f78accc47ec0b3897ab0
Jelle Baele <jellebaele@hotmail.com>
2024-08-21 18:27:07 +0200

    Add Search interface + small refactor BinarySearch

--- src/binary_search/BinarySearch.ts
+++ src/binary_search/BinarySearch.ts
@@ -1,15 +1,18 @@
-export default class BinarySearch<T> {
-  public search(list: Array<T>, target: T): number {
+import ISearch from '../shared/ISearch';
+
+// O(log n)
+export default class BinarySearch<T> implements ISearch<T> {
+  search(sortedArr: T[], element: T): number {
     let low = 0;
-    let high = list.length - 1;
+    let high = sortedArr.length - 1;
 
     while (low <= high) {
-      const index = Math.floor((low + high) / 2);
-      const guess = list[index];
+      const middle = Math.floor((low + high) / 2);
+      const guess = sortedArr[middle];
 
-      if (guess === target) return index;
-      if (guess > target) high = index - 1;
-      if (guess < target) low = index + 1;
+      if (guess === element) return middle;
+      if (guess < element) low = middle + 1;
+      else high = middle - 1;
     }
 
     return -1;
---
+++ src/shared/ISearch.ts
@@ -0,0 +1,5 @@
+interface ISearch<T> {
+  search(arr: T[], element: T): number;
+}
+
+export default ISearch;
