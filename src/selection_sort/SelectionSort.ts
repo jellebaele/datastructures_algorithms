bc5f114e11f1c7d18edec134b472c39eef00503d
Jelle Baele <jellebaele@hotmail.com>
2024-09-23 18:24:01 +0200

    Minor refactoring

--- src/selection_sort/SelectionSort.ts
+++ src/selection_sort/SelectionSort.ts
@@ -4,29 +4,29 @@ import ISort from '../shared/ISort';
 
 export default class SelectionSort<T> implements ISort<T> {
   public sort(arr: Array<T>): Array<T> {
-    const resultArr = new Array<T>();
+    const sortedArr = new Array<T>();
     const arrToSort = [...arr];
 
     for (let i = 0; i < arr.length; i++) {
       const indexSmallest = this.getIndexSmallest(arrToSort);
-      resultArr.push(arrToSort[indexSmallest]);
+      sortedArr.push(arrToSort[indexSmallest]);
       arrToSort.splice(indexSmallest, 1);
     }
 
-    return resultArr;
+    return sortedArr;
   }
 
   private getIndexSmallest(arr: Array<T>): number {
-    let indexSmallest = 0;
-    let smallest = arr[0];
+    let indexSmallestValue = 0;
+    let smallestValue = arr[0];
 
     for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < smallest) {
-        smallest = arr[i];
-        indexSmallest = i;
+      if (arr[i] < smallestValue) {
+        smallestValue = arr[i];
+        indexSmallestValue = i;
       }
     }
 
-    return indexSmallest;
+    return indexSmallestValue;
   }
 }
