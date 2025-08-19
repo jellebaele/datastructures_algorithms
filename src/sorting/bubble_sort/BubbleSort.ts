b2d9f234b425f67369bdf8addbb674a6590f060d
jellebaele <jellebaele@hotmail.com>
2025-08-19 17:43:54 +0200

    Add extra comments

--- src/sorting/bubble_sort/BubbleSort.ts
+++ src/sorting/bubble_sort/BubbleSort.ts
@@ -1,5 +1,13 @@
 import ISort from '../ISort';
 
+/**
+ * How it works:
+ *    1. Go through the array, one value at a time.
+ *    2. For each value, compare the value with the next value.
+ *    3. If the value is higher than the next one, swap the values so that the highest value comes last.
+ *    4. Go through the array as many times as there are values in the array.
+ */
+
 //  O(n^2)
 export default class BubbleSort<T> implements ISort<T> {
   sort(arr: T[]): Array<T> {
--- src/sorting/selection_sort/SelectionSort.ts
+++ src/sorting/selection_sort/SelectionSort.ts
@@ -2,31 +2,38 @@ import ISort from '../ISort';
 
 // O(n^2)
 
+/**
+ * How it works:
+ *    1. Go through the array to find the lowest value.
+ *    2. Move the lowest value to the front of the unsorted part of the array.
+ *    3. Go through the array again as many times as there are values in the array.
+ */
 export default class SelectionSort<T> implements ISort<T> {
-  public sort(arr: Array<T>): Array<T> {
-    const sortedArr = new Array<T>();
+  sort(arr: T[]): T[] {
+    const result = new Array<T>(arr.length);
     const arrToSort = [...arr];
 
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < result.length; i++) {
       const indexSmallest = this.getIndexSmallest(arrToSort);
-      sortedArr.push(arrToSort[indexSmallest]);
+      result[i] = arrToSort[indexSmallest];
       arrToSort.splice(indexSmallest, 1);
     }
 
-    return sortedArr;
+    return result;
   }
 
-  private getIndexSmallest(arr: Array<T>): number {
-    let indexSmallestValue = 0;
-    let smallestValue = arr[0];
+  private getIndexSmallest(arr: T[]): number {
+    let index = 0;
+    let smallest = arr[index];
 
     for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < smallestValue) {
-        smallestValue = arr[i];
-        indexSmallestValue = i;
+      const element = arr[i];
+      if (element < smallest) {
+        index = i;
+        smallest = element;
       }
     }
 
-    return indexSmallestValue;
+    return index;
   }
 }
